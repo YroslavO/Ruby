370d1a9b6cd63d12b83cf93b7c6b034030706ed6
unknown <ogienko41@gmail.com>
2021-04-26 00:38:04 +0300

    v0.3

--- FizzBizz.rb
+++ FizzBizz.rb
@@ -1,1 +1,15 @@
+y= gets.to_i
 
+x=1
+while (x<y)
+  if x%3==0 && x%5==0
+    puts "FizzBizz"
+  elsif x%3==0
+    puts "Fizz"
+  elsif x%5==0
+    puts "Bizz"
+  else
+    puts x
+  end
+  x=x+1
+end
---
+++ Ruby.rb
@@ -0,0 +1,24 @@
+
+puts "Hello Word" # it is some text
+
+=begin
+some text
+=end
+
+age = 30
+puts "Hi my age #{age}"
+
+x = 5
+y = 2
+c = 5.0
+
+puts x-y
+puts x+y
+puts x/y  # int namber
+puts x*y
+puts x%y
+puts x**y
+puts c/y  # double
+
+q = (3+2)*2
+puts q
