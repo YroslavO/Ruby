3dd903cdacc51e8238927b66458613f91a41eac6
unknown <ogienko41@gmail.com>
2021-04-29 01:27:51 +0300

    v0.4

--- arrays.rb
+++ arrays.rb
@@ -8,3 +8,21 @@ for i in (0..y)
   n+=1
   puts nums[n]
 end
+
+nums_2 = [2,6,34,887,1,4,5]
+nums_3=nums_2-nums
+puts nums_3
+puts nums&nums_2
+puts " "
+puts nums|nums_2
+=begin
+.length
+.size
+.sort
+.uniq
+.uniq!
+.freeze
+.include?
+.min
+.max
+=end
